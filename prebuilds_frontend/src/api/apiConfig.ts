e895c46263a155695471354cf98effbc8f482c6d
PreYem <heavenly.onyx401@gmail.com>
2025-03-29 01:58:25 +0000

    changed back to the correct apiConfig

--- prebuilds_frontend/src/api/apiConfig.ts
+++ prebuilds_frontend/src/api/apiConfig.ts
@@ -1,4 +1,4 @@
-export const BASE_API_URL: string = "http://localhost:8000";
+// export const BASE_API_URL: string = "http://localhost:8000";
 
-// export const BASE_API_URL: string = "https://api.prebuilds.shop";
+export const BASE_API_URL: string = "https://api.prebuilds.shop";
 // testing for git ignore
