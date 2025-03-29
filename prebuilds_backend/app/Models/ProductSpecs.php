9ec13d8d045398327ecea48e4763e004d8ab5345
PreYem <heavenly.onyx401@gmail.com>
2025-03-29 18:36:55 +0000

    changed some controller naming to avoid some issues

--- prebuilds_backend/app/Models/ProductSpecs.php
+++ prebuilds_backend/app/Models/ProductSpecs.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Product_Spec extends Model
+class ProductSpecs extends Model
 {
     /**
      * The table associated with the model.
