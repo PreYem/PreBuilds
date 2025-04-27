a11e7d6d8a5186034ccfac7d788aa6139c4e1e47
PreYem <heavenly.onyx401@gmail.com>
2025-04-27 15:33:36 +0100

    removed an offline banner

--- prebuilds_frontend/src/components/TopNavBar.tsx
+++ prebuilds_frontend/src/components/TopNavBar.tsx
@@ -28,8 +28,7 @@ const TopNavbar = () => {
           <Link to="/" className="flex items-center">
             <img src={Logo} alt="Logo" className="h-8 w-10 animate-spin-slow" />
             {/*To be changed later when hosted again*/}
-            <span className="md:inline customFont">{WEBSITE_NAME + " is currently offline, come back later."}</span>{" "}
-            {/*To be changed later when hosted again*/}
+            <span className="md:inline customFont">{WEBSITE_NAME}</span> {/*To be changed later when hosted again*/}
           </Link>
         </div>
         {/* Mobile Menu Toggle (for smaller screens) */}
