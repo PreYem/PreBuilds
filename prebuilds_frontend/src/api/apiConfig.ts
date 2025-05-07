205e9c52d821d59f533b5aef94893302f78e9c60
PreYem <heavenly.onyx401@gmail.com>
2025-05-07 03:09:36 +0100

    fixed a missing export

--- prebuilds_frontend/src/api/apiConfig.ts
+++ prebuilds_frontend/src/api/apiConfig.ts
@@ -2,7 +2,7 @@ const Production_URL = "https://api.prebuilds.shop";
 
 const Development_URL = "http://localhost:8000";
 
-const BASE_API_URL: string = window.location.hostname === "localhost" ? Development_URL : Production_URL;
+export const BASE_API_URL: string = window.location.hostname === "localhost" ? Development_URL : Production_URL;
 
 // Making a simple variable that detects if we're in development mode or production mode, without having to manually 
 // change the base URL before each push on main branch
